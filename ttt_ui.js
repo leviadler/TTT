bdbd46aabacaeea0ac488cb115f2a850baea16c8
Levi Adler <leviadler@gmail.com>
2014-06-12 19:52:25 -0400

    complete game

---
+++ ttt_ui.js
@@ -0,0 +1,46 @@
+// (function (root) {
+//   var TTT = root.TTT = (root.TTT || {});
+//   
+//   var TTTUI = TTT.TTTUI = function(game, $element) {
+//     this.game = game;
+//     this.$element = $element;
+//   };
+//   
+//   
+// })(this);
+
+$(document).ready(function(){
+  
+  var game = new TTT.Game();
+  
+  $('.alert').html(game.player + ": make a move!");
+  
+  $(".grid").on("click", "li", function(event) {
+    var $li = $(this);
+    
+    var row = Math.floor(($li.index()) / 3);
+    var col = Math.floor(($li.index()) % 3);
+    
+    if(game.isEmptyPos([row, col])){
+      if (game.player == "x") {
+        $li.addClass("x");
+      } else {
+        $li.addClass("o");
+      }
+      
+      game.move([row, col]);
+    
+      $('.alert').html(game.player + ": make a move!");
+    
+      if(game.winner()){
+        $('.alert').html(game.player + " has lost!!! Play again!");
+        $('.grid li').removeClass("x");
+        $('.grid li').removeClass("o");
+        game = new TTT.Game();
+      }
+    } else {
+      $('.alert').html("Not a valid move. Try again, " + game.player);
+    }
+  });
+  
+});
